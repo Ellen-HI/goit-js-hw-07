e23a760897d74a523f442367b952bc56f2383560
HELEN <alenagolosova97@gmail.com>
2026-04-03 18:15:54 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+// const { createElement } = require("react");
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -22,5 +24,13 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const gallery = document.querySelector(".gallery");
+const newGallery = images
+  .map(({ url, alt }) => {
+    return `<li> <img src= '${url}' alt'${alt}'></li>`;
+  })
+  .join("");
+gallery.innerHTML = newGallery;
