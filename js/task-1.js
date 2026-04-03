08d4ed3e778395c6d434566f4348434b61b15e84
HELEN <alenagolosova97@gmail.com>
2026-04-03 23:23:44 +0300

    fix bag

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const categories = document.querySelectorAll("li.item");
-console.log(`Numbers of categories: ${categories.length}`);
+console.log(`Number of categories: ${categories.length}`);
 categories.forEach((category) => {
-  const text = category.querySelector("h2.text").textContent;
+  const text = category.querySelector("h2").textContent;
   const liCount = category.querySelectorAll("li").length;
   console.log(`Category: ${text}`);
   console.log(`Quantity: ${liCount}`);
