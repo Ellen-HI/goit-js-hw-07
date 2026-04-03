5fdcd0b437e733c62c008257524236829127819e
HELEN <alenagolosova97@gmail.com>
2026-04-03 20:22:42 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
-
+const btn = document.querySelector(".login-form");
+btn.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cleanEmail = event.target.elements.email.value.trim();
+  const cleanPass = event.target.elements.password.value.trim();
+  if (cleanEmail === "" || cleanPass === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log({
+      email: cleanEmail,
+      password: cleanPass,
+    });
+    event.target.reset();
+  }
+});
